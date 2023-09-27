c66438e2eb72705707c4a1d6a4e864910f74f87b
Lilac <jonasjosh7@gmail.com>
2023-09-27 16:12:35 -0600

    project component completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,18 +3,41 @@ import React from 'react';
 function Project() {
   const projects = [
     {
-      title: 'Project 1',
-      description: 'This is the first project',
-      githubLink: 'https://github.com/project1',
-      deployedLink: 'https://project1.com',
+      title: 'Filmetheca',
+      description: 'A user driven film application. Collaboratively created.',
+      githubLink: 'https://github.com/adamhansen184/filmetheca/',
+      deployedLink: 'https://adamhansen184.github.io/filmetheca/',
     },
     {
-      title: 'Project 2',
-      description: 'This is the second project',
-      githubLink: 'https://github.com/project2',
-      deployedLink: 'https://project2.com',
+      title: 'Pizza Express',
+      description: 'A business centered application, designed with pizza shops in mind. Collaboratively created.',
+      githubLink: 'https://github.com/christianbmartinez/pizza-express',
+      deployedLink: 'https://nameless-river-58365-d5c2867a5c56.herokuapp.com/',
     },
-    // Add more projects as needed
+    {
+        title: 'Tech Blog',
+        description: 'A full stack application designed to provide a space for collaboration and discussion of tech topics. Created solo.',
+        githubLink: 'https://github.com/lilac27/blog',
+        deployedLink: 'https://lilac-blog-c1eaf0c9e1eb.herokuapp.com/',
+      },
+      {
+        title: 'Note Taker',
+        description: 'An individual centered application designed to act as a personal notebook. Created solo.',
+        githubLink: 'https://github.com/lilac27/Note-Taker',
+        deployedLink: 'https://radiant-waters-72762-2c7bc798e78b.herokuapp.com/notes',
+      },
+      {
+        title: 'Weather Checker',
+        description: 'An individual centered application designed to provide weather forecasts for different cities. Created solo.',
+        githubLink: 'https://github.com/lilac27/Weather-Checker',
+        deployedLink: 'https://lilac27.github.io/Weather-Checker/',
+      },
+      {
+        title: 'Work Day Scheduler',
+        description: 'An individual centered application designed to allow scheduling of tasks. Created solo.',
+        githubLink: 'https://github.com/lilac27/Work-Day-Scheduler',
+        deployedLink: 'https://lilac27.github.io/Work-Day-Scheduler/',
+      },
   ];
 
   return (
