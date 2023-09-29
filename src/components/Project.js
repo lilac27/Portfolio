afddd9051dc545b80419c97a9477c7a145541758
Lilac <jonasjosh7@gmail.com>
2023-09-29 01:16:16 -0600

    work done on project/portfolio components

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,44 +1,43 @@
 import React from 'react';
 
-function Project() {
+const Project = () => {
   const projects = [
     {
       title: 'Filmetheca',
-      description: 'A user driven film application. Collaboratively created.',
+      description: 'A user-driven film application. Collaboratively created.',
       githubLink: 'https://github.com/adamhansen184/filmetheca/',
       deployedLink: 'https://adamhansen184.github.io/filmetheca/',
     },
     {
       title: 'Pizza Express',
-      description: 'A business centered application, designed with pizza shops in mind. Collaboratively created.',
+      description: 'A business-centered application, designed with pizza shops in mind. Collaboratively created.',
       githubLink: 'https://github.com/christianbmartinez/pizza-express',
       deployedLink: 'https://nameless-river-58365-d5c2867a5c56.herokuapp.com/',
     },
     {
-        title: 'Tech Blog',
-        description: 'A full stack application designed to provide a space for collaboration and discussion of tech topics. Created solo.',
-        githubLink: 'https://github.com/lilac27/blog',
-        deployedLink: 'https://lilac-blog-c1eaf0c9e1eb.herokuapp.com/',
-      },
-      {
-        title: 'Note Taker',
-        description: 'An individual centered application designed to act as a personal notebook. Created solo.',
-        githubLink: 'https://github.com/lilac27/Note-Taker',
-        deployedLink: 'https://radiant-waters-72762-2c7bc798e78b.herokuapp.com/notes',
-      },
-      {
-        title: 'Weather Checker',
-        description: 'An individual centered application designed to provide weather forecasts for different cities. Created solo.',
-        githubLink: 'https://github.com/lilac27/Weather-Checker',
-        deployedLink: 'https://lilac27.github.io/Weather-Checker/',
-      },
-      {
-        title: 'Work Day Scheduler',
-        description: 'An individual centered application designed to allow scheduling of tasks. Created solo.',
-        githubLink: 'https://github.com/lilac27/Work-Day-Scheduler',
-        deployedLink: 'https://lilac27.github.io/Work-Day-Scheduler/',
-      },
-  ];
+      title: 'Tech Blog',
+      description: 'A full stack application designed to provide a space for collaboration and discussion of tech topics. Created solo.',
+      githubLink: 'https://github.com/lilac27/blog',
+      deployedLink: 'https://lilac-blog-c1eaf0c9e1eb.herokuapp.com/',
+    },
+    {
+      title: 'Note Taker',
+      description: 'An individual centered application designed to act as a personal notebook. Created solo.',
+      githubLink: 'https://github.com/lilac27/Note-Taker',
+      deployedLink: 'https://radiant-waters-72762-2c7bc798e78b.herokuapp.com/notes',
+    },
+    {
+      title: 'Weather Checker',
+      description: 'An individual centered application designed to provide weather forecasts for different cities. Created solo.',
+      githubLink: 'https://github.com/lilac27/Weather-Checker',
+      deployedLink: 'https://lilac27.github.io/Weather-Checker/',
+    },
+    {
+      title: 'Work Day Scheduler',
+      description: 'An individual centered application designed to allow scheduling of tasks. Created solo.',
+      githubLink: 'https://github.com/lilac27/Work-Day-Scheduler',
+      deployedLink: 'https://lilac27.github.io/Work-Day-Scheduler/',
+    },  ];
 
   return (
     <div>
@@ -56,6 +55,6 @@ function Project() {
       ))}
     </div>
   );
-}
+};
 
 export default Project;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Project from './Project';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <div>
+        <Project />
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
