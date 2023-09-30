9bbd44db911f76ca896a2184f45eaa92bf7e0c86
Lilac <jonasjosh7@gmail.com>
2023-09-30 00:51:22 -0600

    working on styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { VscBellDot } from 'react-icons/vsc';
+import '../App.css';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,16 +13,10 @@ function Navbar() {
     setIsOpen(false);
   };
 
+
   return (
     <nav className='flex justify-between items-center p-4'>
         <span className='text-pink-600 font-bold'>Joshua "Lilac" Jonas</span>
-      {/* <div className='hidden md:flex space-x-4'>
-        <Link to='/'>About Me</Link>
-        <Link to='/portfolio'>Portfolio</Link>
-        <Link to='/resume'>Resume</Link>
-        <Link to='/contact'>Contact</Link>
-        <VscBellDot className='text-pink-600 hover:animate-bounce' />
-      </div> */}
       <div className='md:hidden'>
         <button onClick={toggleMenu} className='text-2xl'>
           &#9776;
@@ -31,16 +25,16 @@ function Navbar() {
       {isOpen && (
         <div className='md:hidden fixed inset-0 bg-black z-10'>
           <div className='flex flex-col items-center pt-20'>
-            <Link to='/' onClick={closeMenu} className='mb-4'>
+            <Link to='/' onClick={closeMenu} className='menu-link'>
               About Me
             </Link>
-            <Link to='/portfolio' onClick={closeMenu} className='mb-4'>
+            <Link to='/portfolio' onClick={closeMenu} className='menu-link'>
               Portfolio
             </Link>
-            <Link to='/resume' onClick={closeMenu} className='mb-4'>
+            <Link to='/resume' onClick={closeMenu} className='menu-link'>
               Resume
             </Link>
-            <Link to='/contact' onClick={closeMenu} className='mb-4'>
+            <Link to='/contact' onClick={closeMenu} className='menu-link'>
               Contact
             </Link>
           </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Project = () => {
   const projects = [
     {
@@ -7,36 +8,42 @@ const Project = () => {
       description: 'A user-driven film application. Collaboratively created.',
       githubLink: 'https://github.com/adamhansen184/filmetheca/',
       deployedLink: 'https://adamhansen184.github.io/filmetheca/',
+      image: '../images/filmetheca.png'
     },
     {
       title: 'Pizza Express',
       description: 'A business-centered application, designed with pizza shops in mind. Collaboratively created.',
       githubLink: 'https://github.com/christianbmartinez/pizza-express',
       deployedLink: 'https://nameless-river-58365-d5c2867a5c56.herokuapp.com/',
+      image: '../images/pizza-express.png'
     },
     {
       title: 'Tech Blog',
       description: 'A full stack application designed to provide a space for collaboration and discussion of tech topics. Created solo.',
       githubLink: 'https://github.com/lilac27/blog',
       deployedLink: 'https://lilac-blog-c1eaf0c9e1eb.herokuapp.com/',
+      image: '../images/blog.png'
     },
     {
       title: 'Note Taker',
       description: 'An individual centered application designed to act as a personal notebook. Created solo.',
       githubLink: 'https://github.com/lilac27/Note-Taker',
       deployedLink: 'https://radiant-waters-72762-2c7bc798e78b.herokuapp.com/notes',
+      image: '../images/note-taker.png'
     },
     {
       title: 'Weather Checker',
       description: 'An individual centered application designed to provide weather forecasts for different cities. Created solo.',
       githubLink: 'https://github.com/lilac27/Weather-Checker',
       deployedLink: 'https://lilac27.github.io/Weather-Checker/',
+      image: '../images/weather.png'
     },
     {
       title: 'Work Day Scheduler',
       description: 'An individual centered application designed to allow scheduling of tasks. Created solo.',
       githubLink: 'https://github.com/lilac27/Work-Day-Scheduler',
       deployedLink: 'https://lilac27.github.io/Work-Day-Scheduler/',
+      image: '../images/work-day-scheduler.png'
     },  ];
 
   return (
@@ -45,6 +52,7 @@ const Project = () => {
         <div key={index}>
           <h2>{project.title}</h2>
           <p>{project.description}</p>
+          <img src={project.image} alt={project.title} />
           <p>
             GitHub: <a href={project.githubLink}>{project.githubLink}</a>
           </p>
